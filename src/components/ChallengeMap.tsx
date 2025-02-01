4357bc2d4d21794a368089129ac6b538abdb75a2
af1121 <af1121ic.ac.uk>
2025-02-01 19:34:44 +0000

    api

--- src/components/ChallengeMap.tsx
+++ src/components/ChallengeMap.tsx
@@ -90,7 +90,7 @@ export function ChallengeMap({ participants }: ChallengeMapProps) {
         loadMap();
       } else {
         const script = document.createElement("script");
-        script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY`;
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}`;
         script.async = true;
         script.defer = true;
         script.onload = loadMap;
